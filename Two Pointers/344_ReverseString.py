3fa3a41a448ee2adf3ccffb722301836bfc57c9c
faithinfate19 <faithinfate19@gmail.com>
2025-12-22 13:22:01 +0400

    Added Q344 : Reverse String

---
+++ Two Pointers/344_ReverseString.py
@@ -0,0 +1,22 @@
+#344. Reverse String
+
+"""
+Write a function that reverses a string.
+The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+"""
+
+
+class Solution:
+    def reverseString(self, s):
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+
+            i += 1
+            j -= 1
